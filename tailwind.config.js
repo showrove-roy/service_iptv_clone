a5dbf2f7025d5e4442812ea9b6cb76e1b3246902
Showrove Roy <showrove200@gmail.com>
2023-06-29 20:34:00 +0600

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        pC:"#DBFF00",
+        sC:"#6BD600"
+      }
+    },
   },
   plugins: [],
 }
