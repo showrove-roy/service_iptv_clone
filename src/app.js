d5ccac5ce33a6c236bd0e05eccfca74ce22aaaf3
Showrove Roy <showrove200@gmail.com>
2023-06-30 03:04:28 +0600

    slider done

--- src/app.js
+++ src/app.js
@@ -23,3 +23,53 @@ let blogSliderRev = new Swiper('.swiper--blog__rev__container', {
 blogSliderRev.on('touchEnd', function() {  
  	blogSliderRev.autoplay.start(); 
 });
+
+
+// slider 2
+let blogSlider2 = new Swiper('.swiper--blog__rev__container2', {
+    wrapperClass: 'swiper--blog__wrapper2',
+    slideClass: 'swiper--blog__slide2',
+    touchEventsTarget: 'swiper--blog__wrapper2',
+    slidesPerView: 'auto',
+    direction: 'horizontal',
+    spaceBetween: 0,
+    freeMode: true,  
+    freeModeMomentum: false,
+    freeModeMomentumBounce: false,
+    speed: 3000,
+    autoplay: {
+    	delay: 0,
+      disableOnInteraction: true,
+      
+  	},
+    loop: true,
+    grabCursor: true,
+});
+
+blogSlider2.on('touchEnd', function() {  
+    blogSlider2.autoplay.start(); 
+});
+// slider 3
+let blogSlider3 = new Swiper('.swiper--blog__rev__container3', {
+    wrapperClass: 'swiper--blog__wrapper3',
+    slideClass: 'swiper--blog__slide3',
+    touchEventsTarget: 'swiper--blog__wrapper3',
+    slidesPerView: 'auto',
+    direction: 'horizontal',
+    spaceBetween: 0,
+    freeMode: true,  
+    freeModeMomentum: false,
+    freeModeMomentumBounce: false,
+    speed: 3000,
+    autoplay: {
+    	delay: 0,
+      disableOnInteraction: true,
+      
+  	},
+    loop: true,
+    grabCursor: true,
+});
+
+blogSlider3.on('touchEnd', function() {  
+ 	blogSlider3.autoplay.start(); 
+});
